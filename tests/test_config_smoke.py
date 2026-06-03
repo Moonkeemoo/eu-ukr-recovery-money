24a8752d6f026ed7b236979a9a15e4d406a60481
Taras Romaniuk <tomoonkee@gmail.com>
2026-06-03 16:53:05 +0300

    chore: scaffold project (pyproject, config, smoke test)

---
+++ tests/test_config_smoke.py
@@ -0,0 +1,14 @@
+from recovery import config
+
+
+def test_cpv_in_scope():
+    assert config.cpv_in_scope("45233140-2") is True   # roadworks
+    assert config.cpv_in_scope("71322000-1") is True    # engineering
+    assert config.cpv_in_scope("15800000-6") is False   # food
+    assert config.cpv_in_scope(None) is False
+    assert config.cpv_in_scope("") is False
+
+
+def test_paths_exist_as_config():
+    assert config.CPV_DIVISIONS == ("45", "71", "09", "31", "34")
+    assert str(config.OUT_DIR).endswith("out")
